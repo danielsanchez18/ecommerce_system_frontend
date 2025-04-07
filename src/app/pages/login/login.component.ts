8512cd671c8e779977749b43a64880e18e780807
danielsanchez18 <dsanchez151r@gmail.com>
2025-04-07 18:19:22 -0500

    Refactor login component layout and styling for better user experience

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { AtSign, Lock, LucideAngularModule } from 'lucide-angular';
+import { RouterLink } from '@angular/router';
+import { AtSign, Lock, LucideAngularModule, ShoppingBag } from 'lucide-angular';
 
 @Component({
   selector: 'page-login',
   imports: [
-    LucideAngularModule
+    LucideAngularModule,
+    RouterLink
   ],
   templateUrl: './login.component.html',
 })
-export class PageLogin { 
+export class PageLogin {
 
   readonly AtSign = AtSign;
   readonly Lock = Lock;
+  readonly ShoppingBag = ShoppingBag;
 
 }
