de727393cbd561c2995439cb0acc2a953dc23deb
danielsanchez18 <dsanchez151r@gmail.com>
2025-04-08 01:03:41 -0500

    feat: Add table overview component and integrate it into the dashboard overview page

---
+++ src/app/components/dashboard/table-overview/table-overview.component.ts
@@ -0,0 +1,76 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LucideAngularModule, ShoppingBag, Package2, Tag, UsersRound, TriangleAlert, Search, ChevronDown, TrendingUp } from 'lucide-angular';
+
+@Component({
+  selector: 'component-dashboard-table-overview',
+  imports: [
+    LucideAngularModule,
+    CommonModule, RouterModule
+  ],
+  templateUrl: './table-overview.component.html',
+})
+export class ComponentDashboardTableOverview {
+
+  readonly ShoppingBag = ShoppingBag;
+  readonly Package2 = Package2;
+  readonly Tag = Tag;
+  readonly UsersRound = UsersRound;
+  readonly TriangleAlert = TriangleAlert;
+  readonly Search = Search;
+  readonly ChevronDown = ChevronDown;
+  readonly TrendingUp = TrendingUp;
+
+  categories = [
+    { name: 'Pedidos',
+      icon: ShoppingBag,
+      title: 'Pedidos Recientes',
+      link: 'pedidos',
+      isActive: true,
+      description: 'Revisa los pedidos últimos pedidos realizados'
+    },
+
+    { name: 'Productos',
+      icon: Package2,
+      title: 'Productos Populares',
+      link: 'productos',
+      isActive: false,
+      description: 'Hecha a un vistazo a los productos más populares'
+    },
+
+    { name: 'Categorias',
+      icon: Tag,
+      title: 'Categorias Populares',
+      link: 'categorias',
+      isActive: false,
+      description: 'Revisa las categorías de los productos'
+    },
+
+    { name: 'Usuarios',
+      icon: UsersRound,
+      title: 'Usuarios Recientes',
+      link: 'usuarios',
+      isActive: false,
+      description: 'Revisa el reporte de usuarios y sus roles'
+    },
+
+    { name: 'Denuncias',
+      icon: TriangleAlert,
+      tile: 'Denuncias',
+      link: 'denucias',
+      isActive: false,
+      description: 'Reporte de las denuncias hechas por los usuarios'
+    },
+  ]
+
+  categoryActive: any = this.categories.find(category => category.isActive);
+
+  categorySelect(selectedCategory: any) {
+    this.categories.forEach(category => {
+      category.isActive = category === selectedCategory;
+    });
+    this.categoryActive = selectedCategory;
+  }
+
+}
--- src/app/pages/dashboard/overview/overview-page.component.ts
+++ src/app/pages/dashboard/overview/overview-page.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ComponentDashboardChartsOverview } from '@components/dashboard/charts-overview/charts-overview.component';
 import { ComponentDashboardStatsOverview } from '@components/dashboard/stats-overview/stats-overview.component';
+import { ComponentDashboardTableOverview } from "../../../components/dashboard/table-overview/table-overview.component";
 
 @Component({
   selector: 'page-dashboard-overview',
   imports: [
     RouterModule,
     ComponentDashboardStatsOverview,
-    ComponentDashboardChartsOverview
-  ],
+    ComponentDashboardChartsOverview,
+    ComponentDashboardTableOverview
+],
   templateUrl: './overview-page.component.html',
 })
 export class PageDashboardOverview { }
