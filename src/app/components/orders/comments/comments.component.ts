9c1015350f98d5caf4d71da1f0a5881749f5bedd
danielsanchez18 <dsanchez151r@gmail.com>
2025-04-18 19:46:41 -0500

    fix: correct import path for Comentario interface to match casing

--- src/app/components/orders/comments/comments.component.ts
+++ src/app/components/orders/comments/comments.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Comentario } from '@interfaces/Comments';
+import { Comentario } from '@interfaces/comments';
 import { Info, LucideAngularModule, Send } from 'lucide-angular';
 
 @Component({
